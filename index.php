eb3cc81400756f0431a32adaf9e18f5036a61238
makoto <mmmagic@gmail.com>
2023-03-20 18:03:15 +0900

    update to keep conversation for 5 minutes

--- index.php
+++ index.php
@@ -24,14 +24,11 @@ function send_reply_message($replyToken, $responseText) {
     curl_close($ch);
 }
 
-function send_chatgpt($inputText) {
+function send_chatgpt($message) {
     $apiKey = "OPENAIのAPIキー";
     $requestData = [
         "model" => "gpt-3.5-turbo",
-        "messages" => [
-            ["role" => "system", "content" => "あなたはいつも50文字以内で答えてくれます。"],
-            ["role" => "user", "content" => $inputText], 
-        ]
+        "messages" => $message,
     ];
     
     $ch = curl_init("https://api.openai.com/v1/chat/completions");
@@ -48,6 +45,41 @@ function send_chatgpt($inputText) {
     return $content->{'choices'}[0]->{'message'}->{'content'};
 }
 
+function make_request_message($inputText, $userId) {
+    $filename = $userId . '.txt';
+    $message = [];
+    $message[] = ["role" => "system", "content" => "あなたは100文字程度で分かりやすく答えてくれます。"];
+    if(file_exists($filename)) {
+        $content = file_get_contents($userId . '.txt');
+        $rows = explode("\n", $content);
+        for ($i = 0; $i < count($rows) - 1; $i += 2) {
+            $message[] = ["role" => "user", "content" => str_replace('\n', "\n", $rows[$i])];
+            $message[] = ["role" => "assistant", "content" => str_replace('\n', "\n", $rows[$i + 1])];
+        }
+    }
+    $message[] = ["role" => "user", "content" => $inputText];
+    return $message;
+}
+
+function delete_old_file($userId) {
+    $filename = $userId . '.txt';
+    if(file_exists($filename)) {
+        $expire = strtotime("-5 minutes");
+        $mod = filemtime($filename);
+        if($mod < $expire){
+            unlink($filename);
+        }
+    }
+}
+
+function save_talk($userId, $text, $responseText) {
+    $filename = $userId . '.txt';
+    $fp = fopen($filename,'a');
+    fwrite($fp, str_replace("\n", '\n', $text) . "\n");
+    fwrite($fp, str_replace("\n", '\n', $responseText) . "\n");
+    fclose($fp);
+}
+    
 $inputJsonMsg = file_get_contents('php://input');
 $inputObj = json_decode($inputJsonMsg);
 
@@ -63,6 +95,10 @@ if($messagType != "text"){
 
 $replyToken = $inputObj->{"events"}[0]->{"replyToken"};
 $text = $inputObj->{"events"}[0]->{"message"}->{"text"};
-$responseText = send_chatgpt($text);
+$userId = $inputObj->{"events"}[0]->{"source"}->{"userId"};
+delete_old_file($userId);
+$message = make_request_message($text, $userId);
+$responseText = send_chatgpt($message);
+save_talk($userId, $text, $responseText);
 
 send_reply_message($replyToken, $responseText);
